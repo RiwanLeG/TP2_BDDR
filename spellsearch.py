16c76d77cb61e13c3d7192b26d81910e06d6f9eb
Léo Mérel <leomerel@yahoo.fr>
2021-12-16 15:25:20 -0500

    Update spellsearch.py

--- spellsearch.py
+++ spellsearch.py
@@ -2,6 +2,8 @@ from pyspark import SparkConf,SparkContext
 from pyspark.shell import spark
 import tkinter as tk
 import tkinter.ttk as ttk
+from tkinter import scrolledtext
+from PIL import ImageTk, Image
 
 class Spellsearch:
 
@@ -13,8 +15,10 @@ class Spellsearch:
         self.window.rowconfigure(1, weight=1)
         self.window.columnconfigure(1, weight=1)
 
+        self.mainframe = tk.Frame(self.window)
+        self.mainframe.grid(column=0, row=0, pady=5, columnspan=1)
         # ---------------- Spell level -----------------------
-        self.frame3 = tk.Frame(self.window)
+        self.frame3 = tk.Frame(self.mainframe)
         self.frame3.grid(column=0, row=0, pady=5, columnspan=3)
 
         self.lbl_level = tk.Label(self.frame3, text="Spell level",
@@ -36,12 +40,12 @@ class Spellsearch:
         self.combo_maxLevel.current()
 
         # ---------------- Classes -----------------------
-        self.lbl_classes = tk.Label(self.window, text="Classes",
+        self.lbl_classes = tk.Label(self.mainframe, text="Classes",
                                font=('Helvetica', 12, 'bold'))
-        self.lbl_classes.grid(column=0, row=2, pady=5)
+        self.lbl_classes.grid(column=1, row=2, pady=5)
 
-        self.frame = tk.Frame(self.window)
-        self.frame.grid(column=0, row=3, pady=5, padx=(15, 0))
+        self.frame = tk.Frame(self.mainframe)
+        self.frame.grid(column=1, row=3, pady=5, padx=(15, 0))
 
         self.classes = tk.Listbox(self.frame, selectmode="multiple", height=8, exportselection=0)
         self.classes.pack(side="left", fill="y")
@@ -60,8 +64,8 @@ class Spellsearch:
         self.scrollbar.config(command=self.classes.yview)
         self.scrollbar.pack(side="right", fill="y")
 
-        self.frame2 = tk.Frame(self.window)
-        self.frame2.grid(column=0, row=4, pady=5, padx=(15, 0))
+        self.frame2 = tk.Frame(self.mainframe)
+        self.frame2.grid(column=1, row=4, pady=5, padx=(15, 0))
 
         self.switch_variable = tk.StringVar(value="OR")
         self.btn_or = tk.Radiobutton(self.frame2, text="Or", variable=self.switch_variable,
@@ -71,23 +75,8 @@ class Spellsearch:
         self.btn_or.pack(side="left")
         self.btn_and.pack(side="left")
 
-        # ---------------- Schools -----------------------
-        self.lbl_schools = tk.Label(self.window, text="School",
-                               font=('Helvetica', 12, 'bold'))
-        self.lbl_schools.grid(column=1, row=2, pady=5)
-
-        self.schools = tk.Listbox(self.window, selectmode="multiple", height=8, exportselection=0)
-        self.schools.grid(column=1, row=3, pady=5, padx=(15,0))
-
-        self.schools_names = ["abjuration", "Necromancy", "Enchantment", "Illusion",
-                         "Divination", "Evocation", "Conjuration", "Transmutation"]
-
-        for i in range(len(self.schools_names)):
-            self.schools.insert(i + 1, self.schools_names[i])
-
-
         #---------------- Spell name -----------------------
-        self.frame4 = tk.Frame(self.window)
+        self.frame4 = tk.Frame(self.mainframe)
         self.frame4.grid(column=0, row=5, pady=5, columnspan=4)
 
         self.lbl_name = tk.Label(self.frame4, text="Spell name",
@@ -108,6 +97,60 @@ class Spellsearch:
         self.btn_end.pack(side="left")
         self.btn_anywhere.pack(side="left")
 
+        # ---------------- VSM -----------------------
+
+        self.frame5 = tk.Frame(self.mainframe)
+        self.frame5.grid(column=0, row=6, pady=5, columnspan=4)
+
+        self.lbl_vsm = tk.Label(self.mainframe, text="VSM",
+                                font=('Helvetica', 12, 'bold'))
+        self.lbl_vsm.grid(column=0, row=6)
+
+        self.switch_variable3 = tk.StringVar(value="")
+
+        self.check_v_btn = tk.Checkbutton(self.frame5, text="v", relief="raised", onvalue="V,", offvalue="", height=1,
+                                          width=1, variable=self.switch_variable3,
+                                          indicatoron=False)
+        self.check_v_btn.pack(side="left")
+
+        self.switch_variable4 = tk.StringVar(value="")
+
+        self.check_s_btn = tk.Checkbutton(self.frame5, text="s", relief="raised", onvalue="S,", offvalue="", width=1,
+                                          height=1, variable=self.switch_variable4,
+                                          indicatoron=False)
+        self.check_s_btn.pack(side="left")
+
+        self.switch_variable5 = tk.StringVar(value="")
+
+        self.check_m_btn = tk.Checkbutton(self.frame5, text="m", relief="raised", onvalue="M,", offvalue="", width=1,
+                                          height=1, variable=self.switch_variable5,
+                                          indicatoron=False)
+        self.check_m_btn.pack(side="left")
+
+        # ---------------- Results -----------------------
+        self.frame6 = tk.Frame(self.window)
+        self.frame6.grid(column=5, row=0, pady=5, columnspan=1)
+
+        self.lbl_resultsTitle = tk.Label(self.frame6, text="Results",
+                                  font=('Helvetica', 12, 'bold'))
+        self.lbl_resultsTitle.pack(side="top")
+        self.resultsBox = tk.Listbox(self.frame6, height=8, exportselection=0)
+        self.resultsBox.pack(side="left")
+        self.results = []
+
+        self.scrollbar2 = tk.Scrollbar(self.frame6, orient="vertical")
+        self.scrollbar2.config(command=self.resultsBox.yview)
+        self.scrollbar2.pack(side="left", fill="y")
+
+        self.btn_show_img = ttk.Button(self.frame6, text="Show Image", command=lambda: self.loadImage())
+        self.btn_show_img.pack(side="right")
+
+        # -------------------- Image ----------------------
+        self.frame8 = tk.Frame(self.window)
+        self.frame8.grid(column=6, row=0, pady=5, columnspan=1)
+
+        self.panel = tk.Label(self.frame8)
+        self.panel.pack(side="top")
 
 
         self.btn_submit = ttk.Button(self.window, text="Submit", command=lambda: self.submit())
@@ -128,21 +171,44 @@ class Spellsearch:
     def sqlRequest(self, request):
         # Requête SQL
         spellsRequest = spark.sql(request)
-        spells = [str(row['name']) for row in spellsRequest.collect()]
-        print(spells)
+        self.spells = [str(row['name']) for row in spellsRequest.collect()]
+        print(self.spells)
 
+    def loadImage(self):
+        try:
+            name = self.results[self.resultsBox.curselection()[0]]
+            path = "img/" + name.lower() + ".png"
+        except IndexError:
+            im = Image.open("img/not_found.png")
+
+        try:
+            im = Image.open(path)
+        except (FileNotFoundError, UnboundLocalError):
+            im = Image.open("img/not_found.png")
+
+        img = ImageTk.PhotoImage(im.resize((200, 300), Image.ANTIALIAS))
+        self.panel.configure(image=img)
+        self.panel.image = img
 
     def submit(self):
         classes_selection = list(map(lambda classe: self.classes_names[int(classe)], list(self.classes.curselection())))
-        schools_selection = list(map(lambda school: self.schools_names[int(school)], list(self.schools.curselection())))
+
+        v_value = self.switch_variable3.get()
+        s_value = self.switch_variable4.get()
+        m_value = self.switch_variable5.get()
+
         level_min = self.combo_minLevel.get()
         level_max = self.combo_maxLevel.get()
         OR_or_AND = " " + self.switch_variable.get() + " "
         spell_name = self.entry_name.get()
         spell_name_where = self.switch_variable2.get()
-        print(classes_selection, schools_selection, level_min, level_max, OR_or_AND, spell_name, spell_name_where)
+        print(classes_selection, level_min, level_max, OR_or_AND, spell_name, spell_name_where)
 
         request = "SELECT name FROM spells WHERE "
+
+        if len(classes_selection) != 0:
+            request += "("
+
         for i in range(len(classes_selection)):
             if i != 0:
                 request += OR_or_AND
@@ -152,25 +218,57 @@ class Spellsearch:
             if level_max != "":
                 request += " AND spells." + classes_selection[i] + " <= " + str(level_max)
 
+        if len(classes_selection) != 0:
+            request += ")"
+
         if spell_name != "":
             if len(classes_selection) > 0:
-                request += " AND "
-            where = ""
+                request += " AND ("
+            else :
+                request += "("
+
             if spell_name_where == "begin":
                 request += "spells.name LIKE " + "\'" + spell_name + "%\'"
             elif spell_name_where == "end":
                 request += "spells.name LIKE " + "\'%" + spell_name + "\'"
             elif spell_name_where == "anywhere":
                 request += "spells.name LIKE " + "\'%" + spell_name + "%\'"
+            request += ")"
 
+        if (v_value != "") or (s_value != "") or (m_value != ""):
 
+            if (len(classes_selection) > 0) or (spell_name != ""):
+                request += " AND "
 
+            is_there_a_and = 0
+            if v_value != "" :
+                is_there_a_and = 1
+                request += "(spells.Components LIKE \'%" + v_value + "%\')"
 
+            if s_value != "":
+                if is_there_a_and == 1:
+                    request += " AND "
+                is_there_a_and = 1
+                request += "(spells.Components LIKE \'%" + s_value + "%\')"
 
-        #self.sqlRequest("SELECT name FROM spells WHERE spells.wizard < 5 AND spells.Components = 'V,'")
+            if m_value != "":
+                if is_there_a_and == 1:
+                    request += " AND "
+                request += "(spells.Components LIKE \'%" + m_value + "%\')"
+
         print(request)
         self.sqlRequest(request)
 
+        self.displayResults()
+
+    def displayResults(self):
+        self.resultsBox.delete(0, tk.END)
+        self.results = []
+        i=1
+        for spell in self.spells:
+            self.resultsBox.insert(i, spell)
+            self.results.append(spell)
+            i+=1
 
 
 if __name__ == '__main__':
